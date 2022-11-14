478d7e797522db98cd50f676bc4bcd33b9f356c9
Aestimo K <zerosonly@protonmail.com>
2022-11-14 15:50:31 +0300

    Redcarpet integration working

--- main.rb
+++ main.rb
@@ -5,12 +5,18 @@ require 'redcarpet'
 class Main < Sinatra::Application
 
   get '/' do
+    erb :index, layout: :layout
+  end
+
+  post '/parse_md' do
   	# define basic MD renderer
   	renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
   	markdown = Redcarpet::Markdown.new(renderer, extensions = {})
 
-    @post = markdown.render("This is absolutely *awesome*.")
-    erb :index, layout: :layout
+  	input = params[:md_input]
+  	@post = markdown.render(input)
+
+  	erb :parse_md, layout: :layout
   end
 
 end
